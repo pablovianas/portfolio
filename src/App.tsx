7959546f17dc495888ec6ab1360318780dbc11ba
Pablo Viana <pabloviana2010@gmail.com>
2023-07-24 20:52:56 -0300

    chore: updated app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import { ThemeProvider } from './context'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { ProjectList } from './components/Projects'
 import { Skills } from './components/Skills'
+import { GlobalStyle } from './styles/globalStyle'
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <Hero />
-      <Skills/>
-      <ProjectList/>
-      <Footer/>
-    </>
-  )
+      <ThemeProvider>
+          <GlobalStyle />
+          <Header />
+          <Hero />
+          <Skills />
+          <ProjectList />
+          <Footer />
+      </ThemeProvider>
+  );
 }
 
 export default App
