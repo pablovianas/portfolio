6eb4fdaca601a14d0c01110988c284b786e06a3d
Pablo Viana <pabloviana2010@gmail.com>
2023-07-24 18:01:21 -0300

    chore: initial files

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { ProjectList } from './components/Projects'
+import { Skills } from './components/Skills'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Hero />
+      <Skills/>
+      <ProjectList/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
